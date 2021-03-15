a16d3b6a146b327976bd331289f346c1ab48ff25
jimy-byerley <jimy.byerley@gmail.com>
2021-03-15 16:08:20 +0100

    added declarations for quaterions

--- arrex/glm.py
+++ arrex/glm.py
@@ -10,3 +10,7 @@ for prec, fmt in (('u','I'), ('i','i'), ('f','f'), ('d','d')):
 	for size in range(2,5):
 		type = getattr(glm, prec+'mat'+str(size))
 		_arrex.declare(type, type, size*fmt)
+		
+for prec in ('f', 'd'):
+	type = getattr(glm, prec+'quat')
+	_arrex.declare(type, type, 4*prec)
