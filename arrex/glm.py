56fc4b2349a6df0221f8c58ca342b184356d63c1
jimy byerley <jimy.byerley@gmail.com>
2021-05-16 22:19:26 +0200

    added padding bytes for glm types

--- arrex/glm.py
+++ arrex/glm.py
@@ -1,10 +1,19 @@
 import glm
 from . import _arrex
+from struct import calcsize
 
-for prec, fmt in (('u8', 'B'), ('i8', 'b'), ('u','I'), ('i','i'), ('f','f'), ('d','d')):
+native = 8	# for 64bit machines only
+
+for prec, fmt in (
+			('u8', 'B'), ('i8', 'b'), ('u16','H'), ('i16','h'), ('u64','Q'), ('i64','q'), 
+			('u','I'), ('i','i'), 
+			('f','f'), ('d','d')):
 	for size in range(2,5):
 		type = getattr(glm, prec+'vec'+str(size))
-		_arrex.declare(type, type, size*fmt)
+		pack = size*fmt
+		remain = calcsize(pack) % native
+		if remain:	pack += 'x' * (native - remain)
+		_arrex.declare(type, type, pack)
 
 for prec, fmt in (('u','I'), ('i','i'), ('f','f'), ('d','d')):
 	for size in range(2,5):
