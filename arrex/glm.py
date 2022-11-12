ec42ceca4912ca6ac51bb0aa10e5d62796f7bf5d
jimy-byerley <jimy.byerley@gmail.com>
2022-11-12 14:19:39 +0100

    fixed wrong struct format for glm matrices

--- arrex/glm.py
+++ arrex/glm.py
@@ -1,8 +1,23 @@
 import glm
 from .dtypes import declare, DDTypeExtension
 from struct import calcsize
+import sys
 
-native = 8	# for 64bit machines only
+def machine_wordsize():
+    num_bytes = 0
+    maxint = sys.maxsize
+    while maxint > 0:
+        maxint = maxint >> 8
+        num_bytes += 1
+    return num_bytes
+    
+def align(pack):
+	remain = calcsize(pack) % wordsize
+	if remain:	
+		pack += 'x' * (wordsize - remain)
+	return pack
+
+wordsize = machine_wordsize()
 
 for prec, fmt in (
 			('u8', 'B'), ('i8', 'b'), ('u16','H'), ('i16','h'), ('u64','Q'), ('i64','q'), 
@@ -10,16 +25,13 @@ for prec, fmt in (
 			('f','f'), ('d','d')):
 	for size in range(2,5):
 		type = getattr(glm, prec+'vec'+str(size))
-		pack = size*fmt
-		remain = calcsize(pack) % native
-		if remain:	pack += 'x' * (native - remain)
-		declare(type, DDTypeExtension(type, pack, type))
+		declare(type, DDTypeExtension(type, align(size*fmt), type))
 
 for prec, fmt in (('u','I'), ('i','i'), ('f','f'), ('d','d')):
 	for size in range(2,5):
 		type = getattr(glm, prec+'mat'+str(size))
-		declare(type, DDTypeExtension(type, size*fmt, type))
+		declare(type, DDTypeExtension(type, align(fmt*size**2), type))
 		
-for prec in ('f', 'd'):
-	type = getattr(glm, prec+'quat')
-	declare(type, DDTypeExtension(type, 4*prec, type))
+for fmt in ('f', 'd'):
+	type = getattr(glm, fmt+'quat')
+	declare(type, DDTypeExtension(type, align(fmt*4), type))
